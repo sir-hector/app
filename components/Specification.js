7688152e1fa2045ebf1b818f758df5a94592b5f4
sir-hector <krauskarol7@gmail.com>
2023-04-29 12:41:43 +0200

    add specifications

---
+++ components/Specification.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Specification = () => {
+  return (
+    <View>
+        <Text>Specification List</Text>
+    </View>
+  )
+}
+
+export default Specification
